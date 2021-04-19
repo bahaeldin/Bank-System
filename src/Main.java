1afbd5ed54a8c4b0b36bc19e43c1cc724cb75de5
BAHAELDIN BAHAR <36114314+bahaeldin@users.noreply.github.com>
2021-04-19 21:58:01 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Bahaeldin Bahar
+ * Date : 19/4/2021
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("TEst");
+
+	}
+
+}
