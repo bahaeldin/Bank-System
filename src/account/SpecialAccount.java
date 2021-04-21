9f01ca5df8e8bdbf79a1f0e89999832f1a483001
BAHAELDIN BAHAR <36114314+bahaeldin@users.noreply.github.com>
2021-04-21 18:58:21 +0200

    SpcialAccount class added

---
+++ src/account/SpecialAccount.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package account;
+
+/**
+ * @author bahae
+ *
+ */
+public class SpecialAccount extends Account{
+
+	public SpecialAccount(double balance, int accountNumber) {
+		super(balance, accountNumber);		
+	}
+
+}
