4c5a10f7e7987e6281a4388cb557e909e30b0e19
BAHAELDIN BAHAR <36114314+bahaeldin@users.noreply.github.com>
2021-04-23 10:41:39 +0200

    Fucntion Withdraw and deposit finished

--- src/account/Account.java
+++ src/account/Account.java
@@ -6,7 +6,7 @@ package account;
 
 public class Account {
 	private double balance;
-	private int accountNumber;
+	private String accountNumber;
 
 	/**
 	 * @return the balance
@@ -25,33 +25,45 @@ public class Account {
 	/**
 	 * @return the accountNumber
 	 */
-	public int getAccountNumber() {
+	public String getAccountNumber() {
 		return accountNumber;
 	}
 
 	/**
 	 * @param accountNumber the accountNumber to set
 	 */
-	public void setAccountNumber(int accountNumber) {
+	public void setAccountNumber(String accountNumber) {
 		this.accountNumber = accountNumber;
 	}
 
-	public Account(double balance, int accountNumber) {
+	public Account(double balance, String accountNumber) {
 		this.balance = balance;
 		this.accountNumber = accountNumber;
 	}
 
-	public void withdraw(double amount) {
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public void withdraw(double amount) {
+		if (amount > 0 && amount < balance) {
+			balance -= amount;
+			System.out.println("Done successfully!\nYour Balance is : " + balance + "$");
+		} else {
+			System.out.println("Sorry! You don't have enogh balance");
+		}
 	}
 
 	public void deposit(double amount) {
-
+		if (amount > 0) {
+			balance += amount;
+			System.out.println("Done successfully!\nYour Balance is : " + balance + "$");
+		}
 	}
 
 	@Override
 	public String toString() {
-		return "Account [balance=" + balance + ", accountNumber=" + accountNumber + "]";
+		return "Account Number : " + accountNumber + "\tBalance : " + balance + "$";
 	}
 
 }
--- src/account/SpecialAccount.java
+++ src/account/SpecialAccount.java
@@ -9,8 +9,22 @@ package account;
  */
 public class SpecialAccount extends Account{
 
-	public SpecialAccount(double balance, int accountNumber) {
+	public SpecialAccount(double balance, String accountNumber) {
 		super(balance, accountNumber);		
+	}
+
+	public SpecialAccount() {
+		super();
+	}
+	
+	public void withdraw(double amount) {
+		if (this.getBalance() - amount >= -1000 ) {
+			double newBalance = this.getBalance() - amount;
+			this.setBalance(newBalance);			
+			System.out.println("Done successfully!\nYour Balance is : " + this.getBalance() + "$");
+		} else {
+			System.out.println("Sorry! You don't have enogh balance");
+		}
 	}
 
 }
